81a2d5b10ded30b3c549bdbf12f0d32ce3fdca9f
Joan Roig <joanroigsegura@gmail.com>
2022-03-30 23:44:19 +0200

    feat: execution control

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,25 @@ import * as Main from "./app/main";
 // Express server to expose endpoints
 const app = express();
 
+// Prevent multiple executions at the same time
+let updateRunning = false;
+
 app.get("/update", (req, res) => {
-  Main.run().then(
-    (result) => {
-      res.send(result);
-    },
-    (error) => {
-      res.status(400).send(error.message);
-    }
-  );
+  if (updateRunning) {
+    res.status(400).send("Update already running, please retry in a minute.");
+  } else {
+    updateRunning = true;
+    Main.run()
+      .then(
+        (result) => {
+          res.send(result);
+        },
+        (error) => {
+          res.status(400).send(error.message);
+        },
+      )
+      .finally(() => (updateRunning = false));
+  }
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
